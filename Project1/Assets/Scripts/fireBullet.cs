e59e6a8dcc751cee688908c81da303affe6daa5b
jis1046 <jis1046@rit.edu>
2023-10-17 15:17:01 -0400

    Project 1 Checkpoint 3

--- Project1/Assets/Scripts/fireBullet.cs
+++ Project1/Assets/Scripts/fireBullet.cs
@@ -10,12 +10,20 @@ public class fireBullet : MonoBehaviour
     [SerializeField]
     float speed = 5f;
 
+    
+
+
+    // Screen boundaries
+    float screenWidth;
+    float screenHeight;
+
     // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
             Fire();
+            
         }
     }
 
@@ -23,11 +31,22 @@ public class fireBullet : MonoBehaviour
     {
         GameObject laser = Instantiate(laserBullet, transform.position, Quaternion.identity);
 
+        Vector2 objectPosition = laser.transform.position;
         Vector2 mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
         Vector2 direction = (mousePosition - (Vector2)transform.position).normalized;
 
         // Apply velocity to the bullet in the specified direction
-        laser.transform.Translate(direction * speed * Time.deltaTime);
+
+        Vector2 velocity = direction * speed * Time.deltaTime;
+
+        objectPosition += velocity;
+
+        laser.transform.Translate(velocity);
+
+        laser.transform.position = objectPosition;
+
+
 
     }
+
 }
